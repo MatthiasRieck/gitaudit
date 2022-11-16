5eff6b262bd12b7116156115f66cd861a12dd38a
Matthias Rieck <Matthias.Rieck@icloud.com>
2022-11-16 18:13:57 +0100

    Added Bucket Entry

--- gitaudit/analysis/merge_debt.py
+++ gitaudit/analysis/merge_debt.py
@@ -2,9 +2,16 @@
 in main that have already been merged in a
 release branch
 """
+from __future__ import annotations
+from typing import List
+
+from pydantic import BaseModel, Field
+
 from gitaudit.branch.hierarchy import linear_log_to_hierarchy_log, changelog_hydration
 from gitaudit.git.controller import Git
 from gitaudit.branch.tree import Tree
+
+from gitaudit.git.change_log_entry import ChangeLogEntry
 
 
 def get_head_base_hier_logs(git: Git, head_ref: str, base_ref: str):
@@ -44,3 +51,78 @@ def get_head_base_hier_logs(git: Git, head_ref: str, base_ref: str):
     )
 
     return head_hier_log, base_hier_log
+
+
+class BucketEntry(BaseModel):
+    """Entry for storing changes in an hierarchy branch ready for matching
+    """
+    merge_commit: ChangeLogEntry
+    branch_commits: List[ChangeLogEntry]
+    children: List[BucketEntry] = Field(default_factory=list)
+
+    @property
+    def merge_sha(self):
+        """The sha that was used for merging this bucket
+        (or the fast forward merge commit sha)
+        """
+        return self.merge_commit.sha
+
+    @property
+    def branch_shas(self):
+        """Shas that come in as part for this merge commit
+        """
+        return list(map(lambda x: x.sha, self.branch_commits))
+
+    @property
+    def children_shas(self):
+        """Shas that come in as part of this merge commit but who are themselves
+        buckets with a substructure
+        """
+        return list(map(lambda x: x.merge_commit.sha, self.children))
+
+    @classmethod
+    def from_change_log_entry(
+        cls,
+        entry: ChangeLogEntry,
+        first_parent_line: List[ChangeLogEntry] = None,
+    ):
+        """Create Bucket from a change log entry
+
+        Args:
+            entry (ChangeLogEntry): The entry to be transformed into a bucket
+            first_parent_line (List[ChangeLogEntry], optional): If this is set
+                the first parent line under the entry will be regarded as an
+                additional contributer to the branch commits. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        merge_commit = entry
+
+        branch_commits = []
+        children = []
+
+        if first_parent_line:
+            assert entry.parent_shas[0] == first_parent_line[0].sha
+            commit_lines = [first_parent_line] + entry.other_parents
+        else:
+            commit_lines = entry.other_parents
+
+        for commit_line in commit_lines:
+            for index, commit in enumerate(commit_line):
+                if commit.other_parents:
+                    # sub_branch_off_point
+                    children.append(BucketEntry.from_change_log_entry(
+                        entry=commit,
+                        first_parent_line=commit_line[(index+1):],
+                    ))
+                    break
+
+                # add to commits
+                branch_commits.append(commit)
+
+        return BucketEntry(
+            merge_commit=merge_commit,
+            branch_commits=branch_commits,
+            children=children
+        )
